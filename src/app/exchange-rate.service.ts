4936c07fb5dc2734aedbc1574179faa205d2d7c4
nkfa2013 <92278248+nkfa2013@users.noreply.github.com>
2024-12-19 15:05:19 -0300

    Criando Componente da Pag Inicial
    - Componente inicio criado (app/components/inicio)

--- src/app/exchange-rate.service.ts
+++ src/app/exchange-rate.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExchangeRateService {
 
-  constructor() { }
+export class ExchangeRateService {
+  private apiUrl = 'https://api.exchangerate-api.com/v4/latest/USD';
+
+  constructor(private http: HttpClient) { }
+
+  getExchangeRates(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  convertCurrency(from: string, to: string, amount: number): Observable<any> {
+    
+    // Implementar lógica de conversão
+  }
 }
