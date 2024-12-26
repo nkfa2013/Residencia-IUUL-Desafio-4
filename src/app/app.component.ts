3ab3e0770f5e6a4b7e14dce507be2b1717a8c15c
nkfa2013 <nkfa2013@gmail.com>
2024-12-26 09:48:49 -0300

    Correção do Projeto
    - Correção do projeto para o padrão da versão 19 do angular, onde os componentes são StandAlone
    - Configurando componente inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from "./components/inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
-  template: '<h1>Bem-vindo ao Conversor Monetário</h1><p>Esta aplicação permite converter valores monetários entre diferentes moedas.</p>',
+  template: '',
 })
 export class InicioComponent {
 
